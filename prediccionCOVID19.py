d56a91a8dccc880c02703e059e3730f912653254
Lydiaprib <lydia.prado@alu.uclm.es>
2020-05-02 22:21:05 +0200

    DataFrame Procesado

--- prediccionCOVID19.py
+++ prediccionCOVID19.py
@@ -14,53 +14,24 @@ from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import train_test_split
 
 from sklearn.metrics import mean_squared_error, r2_score
-'''from german_holidays import get_german_holiday_calendar'''
 sys.path.insert(0, 'C:/Users/Lydia Prado Ibáñez/git/TrabajoPrediccionCOVID19')
+pd.set_option("display.max_rows", None)
+
+from pruebaMatlab import PruebaMatlab
 
 
 #Importacion de los datos
-df = pd.read_excel("SalesData.xlsx", parse_dates = True, squeeze = True)
-test = pd.read_excel("predictionEmpty.xlsx", parse_dates = True, squeeze = True)
+pm = PruebaMatlab()
+dfOriginal = pm.dataFrame() #Carga DF con los datos originales
+test = pd.read_excel("Predicciones/CLM_15Abril.xlsx", parse_dates = True, squeeze = True)
+dfOriginal = dfOriginal.iloc[20:]
+dfOriginal["Day"] = pd.to_datetime(dfOriginal["Date"]).dt.day
+dfOriginal["Week"] = pd.to_datetime(dfOriginal["Date"]).dt.week
+dfOriginal["WeekDay"] = pd.to_datetime(dfOriginal["Date"]).dt.dayofweek
+dfOriginal["Month"] = pd.to_datetime(dfOriginal["Date"]).dt.month
 
-#Añadir más informacion al DF original para poder aplicar RF
-df["Year"] = pd.to_datetime(df["Date"]).dt.year
-df["Week"] = pd.to_datetime(df["Date"]).dt.week
-df["Day"] = pd.to_datetime(df["Date"]).dt.day
-df["WeekDay"] = pd.to_datetime(df["Date"]).dt.dayofweek
 
-print(df.head())
+print(dfOriginal)
 
-'''#Uso de Six-Sigma
-sns.set(rc = {"figure.figsize": (16.7, 10)})
-sns.boxplot(x = df["Sold Units"])
-sns.lineplot(df["Week"], df["Sold Units"])'''
 
-#Division de los datos para seleccionar los componentes y los predictores
 
-predictors = df.drop(["Date", "Sold Units"], axis = 1)
-target = df["Sold Units"]
-x_train, x_cv, y_train, y_cv = train_test_split(predictors, target, test_size = 0.4, random_state = 1)
-
-#Creacion del modelo con RF
-model = RandomForestRegressor(n_estimators = 500, oob_score = True, n_jobs = 1, random_state = 7, max_features = "auto", min_samples_leaf = 4)
-model.fit(x_train, y_train)
-pred = model.predict(x_cv)
-
-#R2 Score
-print(r2_score(pred, y_cv))
-
-def mean_percentage_error(y_cv, pred):
-    y_cv, pred = np.array(y_cv), np.array(pred)
-    return np.mean(np.array((y_cv - pred) / y_cv)) * 100
-
-print(mean_percentage_error(y_cv, pred))
-
-test["Year"] = pd.to_datetime(test["Date"]).dt.year
-test["Week"] = pd.to_datetime(test["Date"]).dt.week
-test["Day"] = pd.to_datetime(test["Date"]).dt.day
-test["WeekDay"] = pd.to_datetime(test["Date"]).dt.dayofweek
-
-test1 = test.drop(["Sales", "Date"], axis = 1)
-pred2 = model.predict(test1)
-test["Sales"] = pred2.round(0)
-print(test.head(20))
--- pruebaMatlab.py
+++ pruebaMatlab.py
@@ -1,18 +1,67 @@
-
 import matlab.engine
+import pandas as pd
+
+
 ## Prueba de lectura de función Matlab ##
 
+class PruebaMatlab():
 
-eng = matlab.engine.start_matlab()
-[output, name_ccaa, iso_ccaa, data_spain] = eng.HistoricDataSpain(nargout=4)
+    def __init__(self):
+        algo = 0
 
-#output.historic(7)
+    def dataFrame(self):
 
-#print(output.historic(7))
-#print(output)
+        #columns = ["AcumulatedCases", "AcumulatedPRC", "AcumulatedTestAc", "Hospitalized", "Critical", "Deaths", "AcumulatedRecoveries", "label_x", "Cases", "DailyCases", "DailyDeaths", "DailyRecoveries"]
+        #columns = ["AcumulatedCases", "AcumulatedPRC"]
 
-a = output
+        #Preparacion del DataFrame
 
-print(a)
+        eng = matlab.engine.start_matlab()
+        [output, name_ccaa, iso_ccaa, data_spain] = eng.HistoricDataSpain(nargout=4)
+        eng.quit()
+        dictCM = output["historic"][6]
 
-eng.quit()
+        dfFechas = pd.DataFrame(dictCM["label_x"])
+        dfFechas = dfFechas
+        dfFechas.columns = ["label_x"]
+
+        dfCases = pd.DataFrame(dictCM["Cases"])
+        dfCases = dfCases.transpose()
+        dfCases.columns = ["Cases"]
+
+        dfHospitalized = pd.DataFrame(dictCM["Hospitalized"])
+        dfHospitalized = dfHospitalized.transpose()
+        dfHospitalized.columns = ["Hospitalized"]
+
+        dfCritical = pd.DataFrame(dictCM["Critical"])
+        dfCritical = dfCritical.transpose()
+        dfCritical.columns = ["Critical"]
+
+        dfDeaths = pd.DataFrame(dictCM["Critical"])
+        dfDeaths = dfDeaths.transpose()
+        dfDeaths.columns = ["Deaths"]
+
+        dfRecoveries = pd.DataFrame(dictCM["AcumulatedRecoveries"])
+        dfRecoveries = dfRecoveries.transpose()
+        dfRecoveries.columns = ["Recoveries"]
+
+        dfFinal = pd.DataFrame(dfFechas.values, columns = ["Date"])
+        dfFinal["Cases"] = dfCases.values
+        dfFinal["Hospitalized"] = dfHospitalized.values
+        dfFinal["Critical"] = dfCritical.values
+        dfFinal["Deaths"] = dfDeaths.values
+        dfFinal["Recoveries"] = dfRecoveries.values
+
+        dfFinal.Cases = dfFinal.Cases.astype(int)
+        dfFinal.Hospitalized = dfFinal.Hospitalized.astype(int)
+        dfFinal.Critical = dfFinal.Critical.astype(int)
+        dfFinal.Deaths = dfFinal.Deaths.astype(int)
+        dfFinal.Recoveries = dfFinal.Recoveries.astype(int)
+        dfFinal["Date"] = pd.to_datetime(dfFinal["Date"], format = "%d-%m-%Y")
+
+        #print(dfFinal)
+        return dfFinal
+
+'''pm = PruebaMatlab()
+
+df = pm.dataFrame'''
