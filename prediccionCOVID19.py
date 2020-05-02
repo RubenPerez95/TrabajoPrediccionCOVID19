2c1861ca5a4fbc9032f744063f4b59c2d3b1af7b
Lydiaprib <lydia.prado@alu.uclm.es>
2020-05-02 13:03:41 +0200

    Importacion de librerias

---
+++ prediccionCOVID19.py
@@ -0,0 +1,14 @@
+#Librerias necesarias
+import pandas as pd
+import numpy as np
+from datetime import datetime
+import matplotlib.pyplot as plt
+import seaborn as sns
+from skelearn.metrics import mean_squared_error
+from german_holidays import get_german_holiday_calendar
+from pandas.tseries.offsets import *
+import sys
+sys.path.insert(0, 'C:/Users/Lydia Prado Ibáñez/git/TrabajoPrediccionCOVID19')
+
+
+
