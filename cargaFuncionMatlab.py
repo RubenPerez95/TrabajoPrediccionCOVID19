61082f5d09f3836fbfac86bb1651d44e700cccc5
Lydiaprib <lydia.prado@alu.uclm.es>
2020-05-06 01:09:22 +0200

    Código refactorizado y comentarios añadidos

--- cargaFuncionMatlab.py
+++ cargaFuncionMatlab.py
@@ -2,25 +2,36 @@ import matlab.engine
 import pandas as pd
 
 
-## Prueba de lectura de función Matlab ##
+class CargaFuncionMatlab():
 
-class PruebaMatlab():
-
+    #Inicialización de la clase
     def __init__(self):
 
         eng = matlab.engine.start_matlab()
         [self.output, self.name_ccaa, self.iso_ccaa, self.data_spain] = eng.HistoricDataSpain(nargout=4)
         eng.quit()
 
-    def dataFrame(self):
 
-        #Preparacion del DataFrame
+    '''
+    Este bucle recorre los datos producidos por la función de matlab y crea un dataframe para cada comunidad autónoma
+    con las variables necesarias para realizar las predicciones.
+    
+    Devuelve una lista que contiene todos los dataframes de las CCAA
+    '''
+    def listaCCAA(self):
+
+        #Declaracion de la lista que va a contener los dataframes de las CCAA
         listaCCAA = [None] * len(self.output["historic"])
 
+        #Bucle para recorrer el diccionario con los datos de las CCAA
         for i in range(len(self.output["historic"])):
+            #Selecciona el diccionario de una Comunidad Autónoma
             dictionary = self.output["historic"][i]
+            #Selecciona el identificador de esa comunidad
             comunidad = self.iso_ccaa[i]
 
+            '''Bloque para obtener las variables a predecir para la comunidad autónoma
+            y guardar los datos en un dataframe'''
             dfFechas = pd.DataFrame(dictionary["label_x"])
             dfFechas = dfFechas
             dfFechas.columns = ["label_x"]
@@ -44,7 +55,10 @@ class PruebaMatlab():
             dfRecoveries = pd.DataFrame(dictionary["DailyRecoveries"])
             dfRecoveries = dfRecoveries.transpose()
             dfRecoveries.columns = ["DailyRecoveries"]
+            '''Fin del bloque'''
 
+            '''Bloque para añadir cada variable a un dataframe final que
+            contine todas las variables de una comunidad'''
             dfFinal = pd.DataFrame(dfFechas.values, columns = ["Date"])
             dfFinal["CCAA"] = comunidad
             dfFinal["DailyCases"] = dfCases.values
@@ -52,14 +66,19 @@ class PruebaMatlab():
             dfFinal["Critical"] = dfCritical.values
             dfFinal["DailyDeaths"] = dfDeaths.values
             dfFinal["DailyRecoveries"] = dfRecoveries.values
+            '''Fin del bloque'''
 
+            '''Bloque que parsea los datos a su formato específico'''
             dfFinal.DailyCases = dfFinal.DailyCases.astype(int)
             dfFinal.Hospitalized = dfFinal.Hospitalized.astype(int)
             dfFinal.Critical = dfFinal.Critical.astype(int)
             dfFinal.DailyDeaths = dfFinal.DailyDeaths.astype(int)
             dfFinal.DailyRecoveries = dfFinal.DailyRecoveries.astype(int)
             dfFinal["Date"] = pd.to_datetime(dfFinal["Date"], format = "%d-%m-%Y")
+            '''Fin del bloque'''
 
+            #Guarda el dataframe generado en la lista que contiene los dataframes de todas las CCAA
             listaCCAA[i] = dfFinal
+
         return listaCCAA
 
